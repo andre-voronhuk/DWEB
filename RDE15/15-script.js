61cb370c9f560b2d421eca4d14ee619241e21484
André Voronhuk <andrevoronhuk@gmail.com>
2021-06-16 17:04:57 -0300

    adicao de estrutura condicional nos dados do usuario

--- RDE15/15-script.js
+++ RDE15/15-script.js
@@ -10,4 +10,19 @@
      elemento = document.getElementById("idade")
      elemento.innerHTML = idade
 
+
+     if (idade < 18) {
+         document.getElementById("maioridade").innerHTML = "não "
+     }
+ }
+
+ function calcular() {
+     n1 = document.getElementById("n1").value
+     n2 = document.getElementById("n2").value
+     resultado = document.getElementById("resultado")
+     resultado.innerHTML = parseFloat(n1) + parseFloat(n2)
+     document.getElementById("n1").value = ""
+     document.getElementById("n2").value = ""
+
+
  }

36c6db71e2f1bb135d416ffaed69368a7292409c
André Voronhuk <andrevoronhuk@gmail.com>
2021-06-16 13:00:33 -0300

    implementando a primeira parte do RDE15

---
+++ RDE15/15-script.js
@@ -0,0 +1,13 @@
+ nome = prompt("Bem Vindo!\nInsira seu nome por favor")
+ idade = prompt("Bem Vindo!\nInsira sua idade")
+
+ function carregado() {
+
+
+     elemento = document.getElementById("nome")
+     elemento.innerHTML = nome
+
+     elemento = document.getElementById("idade")
+     elemento.innerHTML = idade
+
+ }
